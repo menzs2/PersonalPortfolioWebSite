dd939030ef5c96a6bf85fd005bddc4cfb42d4524
77menzs <77menzs@gmail.com>
2025-04-08 13:59:12 +0200

    Add Skill model class

--- PersonalPortfolioWebSite/Models/Project.cs
+++ PersonalPortfolioWebSite/Models/Project.cs
@@ -1,7 +1,10 @@
-﻿namespace PersonalPortfolioWebSite;
+﻿using System.Collections.Generic;
+
+namespace PersonalPortfolioWebSite;
 
 public class Project
 {
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+    public List<Skill> Technologies { get; set; } = new List<Skill>();
 }
---
+++ PersonalPortfolioWebSite/Models/Skill.cs
@@ -0,0 +1,10 @@
+﻿namespace PersonalPortfolioWebSite;
+
+public class Skill
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Icon { get; set; } = string.Empty;
+    public int Level { get; set; } = 0;
+    public string Category { get; set; } = string.Empty;    
+}
