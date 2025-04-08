e37d73fb63bf7ed0ebf0636a0b90b43d7a796a00
77menzs <77menzs@gmail.com>
2025-04-08 15:46:19 +0200

    Corrections

--- PersonalPortfolioWebSite/Models/SkillModel.cs
+++ PersonalPortfolioWebSite/Models/SkillModel.cs
@@ -1,10 +1,18 @@
 ﻿namespace PersonalPortfolioWebSite.Models;
 
+using System.ComponentModel.DataAnnotations;
+
 public class SkillModel
 {
+    [Required(ErrorMessage = "Name is required.")]
+    [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters.")]
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public string Icon { get; set; } = string.Empty;
-    public int Level { get; set; } = 0;
-    public string Category { get; set; } = string.Empty;    
+
+    [Range(1, 10, ErrorMessage = "Level must be between 1 and 10.")]
+    public int Level { get; set; } = 1;
+
+    public string Category { get; set; } = string.Empty;
+    public int YearsOfExperience { get; set; } = 1;
 }
