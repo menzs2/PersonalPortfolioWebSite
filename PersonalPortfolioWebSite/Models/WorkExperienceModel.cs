b721c4fdca9c9c144d69a95b3bd35c117148a180
77menzs <77menzs@gmail.com>
2025-04-15 15:38:50 +0200

    Add modelt work experience

---
+++ PersonalPortfolioWebSite/Models/WorkExperienceModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonalPortfolioWebSite.Models;
+
+public class WorkExperienceModel
+{
+    public string CompanyName { get; set; } = string.Empty;
+    public string JobTitle { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public List<SkillModel> Technologies { get; set; } = new List<SkillModel>();
+    public string Location { get; set; } = string.Empty;
+}
